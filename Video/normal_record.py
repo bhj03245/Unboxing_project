6bf88d9f673dd40ad1898b6e2f0e444b0ac90753
wk1219 <sjms1215@naver.com>
2020-03-10 21:25:15 +0900

    update normal_record

--- Video/normal_record.py
+++ Video/normal_record.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+#-*- coding: utf-8 -*-
 import cv2
 import os
 import time
@@ -22,7 +22,8 @@ gp.output(7, False)
 gp.output(11, False)
 gp.output(12, True)
 
-path = os.getcwd() + '/UB_video/Normal/'
+norm_path = os.getcwd() + '/UB_video/Normal/'
+manl_path = os.getcwd() + '/UB_video/Manual/'
 
 picam = PiCamera()
 picam.resolution = (640,480)
@@ -32,7 +33,7 @@ rawCapture = PiRGBArray(picam, size=(640,480))
 time.sleep(0.1)
 
 def create_time():
-    now = datetime.datetime.today().strftime("%Y%m%d_%H%M%S")
+    now = datetime.datetime.today().strftime("%y%m%d_%H%M%S")
     return now
 
 def create_file():
@@ -40,32 +41,49 @@ def create_file():
     file_name = now + '.h264'
     return file_name
 
-def convert(file_name):
+def convert(path, file_name):
     conv_file = file_name.split('.')[0] + '.mp4'
     convert_cmd = 'MP4Box -fps 25 -add ' + path + file_name + " " + path + conv_file + "; rm " + path + file_name
     os.system(convert_cmd)
 
-def normal_recording(picam):
-    file_name = create_file()
-    picam.start_recording(path + file_name)
-    picam.wait_recording(10)
-    picam.stop_recording()
-    convert(file_name)
-    nthread = threading.Thread(target=normal_recording, args=(picam,))
-    nthread.start()
+class recording:
+	def normal_recording(self, picam):
+		key = int(input("Interrupt : "))
+		if key == 1:
+			print("Manual ON")
+			self.manual_recording(picam)
+		
+		__file_name = create_file()
+		file_name = "NORM_" + __file_name		
+		picam.start_recording(norm_path + file_name)
+		picam.wait_recording(10)
+		picam.stop_recording()
+		convert(norm_path, file_name)
+		nthread = threading.Thread(target=self.normal_recording, args=(picam,))
+		nthread.start()
+		print("Finish")
+        
+	
+	def manual_recording(self, picam):
+		__file_name = create_file()
+		file_name = "MANL_" + __file_name
+		picam.start_recording(manl_path + file_name)
+		picam.wait_recording(10)
+		picam.stop_recording()
+		convert(manl_path, file_name)
+		nthread = threading.Thread(target=self.manual_recording, args=(picam,))
+		nthread.start()
+		print("Manual Finish")
+  	
 
+	def recording(self):
+		frame = picam.start_preview(fullscreen=False, window=(100, 20, 640, 480))
+		self.normal_recording(picam)
 
-def video():
-    now = create_time()
-    file_name = create_file()
-    picam.start_preview()
-    normal_recording(picam)
-    
-    key = cv2.waitKey(1) & 0xFF
-        #rawCapture.truncate(0)
-        # if key == ord("q"):     
+	    
 
-video()
+r = recording()
+r.recording()
 
 
 #nthread = threading.Thread(target=normal, args=(picam,))
