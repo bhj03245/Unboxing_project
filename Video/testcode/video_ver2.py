1d068cf1d5a3731cbd22ef90550d01895a985968
wk1219 <sjms1215@naver.com>
2020-03-12 19:15:04 +0900

    update video_ver2

--- Video/testcode/video_ver2.py
+++ Video/testcode/video_ver2.py
@@ -25,8 +25,9 @@ gp.output(12, True)
 norm_path = os.getcwd() + '/UB_video/Normal/'
 manl_path = os.getcwd() + '/UB_video/Manual/'
 
-#fourcc = cv2.cv.CV_FOURCC(*'H264')
-fourcc = cv2.VideoWriter_fourcc(*'X264')
+fourcc = cv2.VideoWriter_fourcc(*'H264')
+
+
 def create_time():
     now = datetime.datetime.today().strftime("%y%m%d_%H%M%S")
     return now
@@ -43,26 +44,13 @@ def convert(path, file_name):
 
 class recording:
 	
-	def normal_recording(self, picam, frame, size):	
-		while int(size) > 3000 :
-			__file_name = create_file()
-			path = norm_path + "NORM_" + __file_name	
-			out = cv2.VideoWriter(path, fourcc, 25.0, (640,480))						
-			out.write(frame)
-			size = 0
-			print("hi")
-			
-		
-		#__file_name = create_file()
-		#file_name = norm_path + "NORM_" + __file_name
-		#return file_name	
-	
-	#	convert(norm_path, file_name)
-	#	nthread = threading.Thread(target=self.normal_recording, args=(picam,))
-	#	nthread.start()
-	#	print("Finish")
-	
+	def normal_recording(self):	
+		__file_name = create_file()
+		path = norm_path + "NORM_" + __file_name
+		norm_out = cv2.VideoWriter(path, fourcc, 25.0, (640,480))
+		return path, norm_out		
 
+	
 	def recording(self): 	
 		picam = cv2.VideoCapture(0)
 		if picam.isOpened() == False:
@@ -72,28 +60,32 @@ class recording:
 		cv2.namedWindow('CAM_Window')
 		cnt = 0
 		prevTime = 0	
-		path = '/home/pi/Desktop/UB_video/Normal/output.h264' # add
-		out = cv2.VideoWriter(path, fourcc, 25.0, (640,480))
+
+		path = self.normal_recording()[0]
+		out = self.normal_recording()[1]	
 		while(True):
 			ret, frame = picam.read()
 			curTime = time.time()
 			sec = curTime - prevTime
 			prevTime = curTime
 			fps = 1/(sec)
+			cnt = cnt + fps
 			str = "FPS : %0.1f" % fps
 			cv2.putText(frame, str, (0,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
 			cv2.imshow('CAM_Window', frame)
-			cnt = cnt + fps
 			print(cnt)	
-			 # add 
-			out.write(frame) # add	
-			#self.normal_recording(picam, frame, cnt)	
-			if cv2.waitKey(10) >= 0:
+			out.write(frame) 
+			if int(cnt) > 2000:		# size (frame * sec)
 				break
-			
+			elif cv2.waitKey(10) >= 0:
+				break
+
+		convert(norm_path, path.split('/')[6])
 		picam.release()
 		out.release()
-		cv2.destroyWindow('CAM_Window')
+		nthread = threading.Thread(target=self.recording, args=())
+		nthread.start()
+		#cv2.destroyWindow('CAM_Window')
 
 r = recording()
 r.recording()
