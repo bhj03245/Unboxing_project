03a12780ccc56fcd5cfefb3530c0299acf44af87
dkrmrm878 <dkrmrm878@naver.com>
2020-03-17 16:21:50 +0900

    add DownloadTest

---
+++ Android/DownloadTest/app/src/main/java/kr/ac/kpu/ce2015150012/downloadtest/MainActivity.java
@@ -0,0 +1,97 @@
+package kr.ac.kpu.ce2015150012.downloadtest;
+
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText downloadUrl;
+    private Button addToQueueButton;
+    private Button cancelLatestButton;
+    private Button viewDownloadsButton;
+
+    private long latestId = -1;
+
+    private DownloadManager downloadManager;
+    private DownloadManager.Request request;
+    private Uri urlToDownload;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        downloadUrl = (EditText)findViewById(R.id.downloadUrl);
+        addToQueueButton = (Button)findViewById(R.id.addQueueButton);
+        cancelLatestButton = (Button)findViewById(R.id.cancelDownloadButton);
+        viewDownloadsButton = (Button)findViewById(R.id.viewDownloadsButton);
+
+        downloadManager = (DownloadManager)getSystemService(Context.DOWNLOAD_SERVICE);
+
+        addToQueueButton.setOnClickListener(this);
+        cancelLatestButton.setOnClickListener(this);
+        viewDownloadsButton.setOnClickListener(this);
+
+    }
+
+    private BroadcastReceiver completeReceiver = new BroadcastReceiver(){
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "다운로드가 완료되었습니다.",Toast.LENGTH_SHORT).show();
+        }
+
+    };
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.addQueueButton:
+
+                List<String> pathSegments = urlToDownload.getPathSegments(); urlToDownload = Uri.parse(downloadUrl.getText().toString());
+                request = new DownloadManager.Request(urlToDownload);
+                request.setTitle("다운로드 예제");
+                request.setDescription("항목 설명");
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, pathSegments.get(pathSegments.size()-1));
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).mkdirs();
+                latestId = downloadManager.enqueue(request);
+                downloadUrl.setText("");
+                break;
+
+            case R.id.cancelDownloadButton:
+                downloadManager.remove(latestId);
+                break;
+
+            case R.id.viewDownloadsButton:
+                startActivity(new Intent(DownloadManager.ACTION_VIEW_DOWNLOADS));
+                break;
+        }
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        IntentFilter completeFilter = new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        registerReceiver(completeReceiver, completeFilter);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        unregisterReceiver(completeReceiver);
+    }
+}
