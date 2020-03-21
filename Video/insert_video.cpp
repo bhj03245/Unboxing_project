d1d7b9b66794afc372692a01f5490e021bd8aef8
wk1219 <sjms1215@naver.com>
2020-03-21 22:57:06 +0900

    update insert video

--- Video/insert_video.cpp
+++ Video/insert_video.cpp
@@ -82,7 +82,9 @@ public:
 
 void db_insert(Video vid);
 void db_select();
+void db_update();
 char* timeToString(struct tm *t);
+
 void db_insert(Video vid) {
 	MYSQL mysql;
 	MYSQL_RES* res;
@@ -133,6 +135,26 @@ void db_insert(Video vid) {
 	mysql_free_result(res);
 	mysql_close(&mysql);
 }
+void db_update(){
+	MYSQL mysql;
+	MYSQL_RES* res;
+	MYSQL_ROW row;
+	int fields;
+	int i;
+	list <string> vlist;
+	list<string>::iterator iter;
+	MYSQL_FIELD* field;
+	char buf[200];
+	char set[20];
+	mysql_init(&mysql);
+	
+	mysql_real_connect(&mysql, DB_HOST, DB_USER, DB_PW, DB_NAME, 3306, NULL, 0);
+	sprintf(buf, "update video.norm set video.norm.norm_num = @cnt:=@cnt+1");
+	sprintf(set, "set @cnt = 0");
+	mysql_query(&mysql, set);
+	mysql_query(&mysql, buf);
+	cout << "Update Success!!" << endl;
+}
 void FileList()
 {
 	DIR *dir = NULL;
@@ -182,6 +204,7 @@ void FileList()
 				
 				vid.setVideo(num, file, buf.st_size, vidLength, vidMakeTime, vidResolution, newpath);	// Set
 				vid.printInfo();	// Output Video Info 
+				db_update();
 				db_insert(vid);
 				// db_select();
 				num++;
