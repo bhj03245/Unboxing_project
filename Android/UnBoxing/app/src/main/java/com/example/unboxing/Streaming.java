cb621cc7ad4f8807ea763c9295a651d470c05074
dkrmrm878 <dkrmrm878@naver.com>
2020-04-13 23:15:42 +0900

    add UnBoxing

---
+++ Android/UnBoxing/app/src/main/java/com/example/unboxing/Streaming.java
@@ -0,0 +1,23 @@
+package com.example.unboxing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class Streaming extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_streaming);
+
+        String uri = "rtsp://125.142.56.46:8554/unicast";
+        VideoView v = (VideoView) findViewById( R.id.videoView );
+        v.setVideoURI( Uri.parse(uri) );
+        //v.setMediaController( new MediaController( this ) );
+        v.requestFocus();
+        v.start();
+    }
+}
