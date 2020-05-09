ece220231cf2ab06f5c378defad97c24acc0e3e6
wk1219 <sjms1215@naver.com>
2020-05-09 18:01:29 +0900

    update park img

--- Video/park_img.py
+++ Video/park_img.py
@@ -8,7 +8,7 @@ rev = True
 img_path = '/var/www/html/Upload/Parkimg'
 
 while cnt == 0:
-	cv2.imwrite(img_path + '/parking_%d.jpg' % cnt, image)
+	cv2.imwrite(img_path + '/capture_%d.jpg' % cnt, image)
 	rev, image = cap.read() 
 	print("save parking image")
 	cnt += 1
