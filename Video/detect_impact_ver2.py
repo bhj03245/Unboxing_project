a6fae92c22d3fb3ab70aff818b7e8b3b0d5d672f
wk1219 <sjms1215@naver.com>
2020-06-20 02:24:05 +0900

    Update detect_impact_ver2.py

--- Video/detect_impact_ver2.py
+++ Video/detect_impact_ver2.py
@@ -6,14 +6,10 @@ import datetime
 import smbus
 import sysv_ipc
 import Queue
+from impt import detect_impact
 
 norm_path = os.getcwd() + '/UB_video/Normal/'
 impt_path = os.getcwd() + '/UB_video/Impact/'
-
-power_mgmt_1 = 0x6b
-bus = smbus.SMBus(1)
-address = 0x68
-bus.write_byte_data(address, power_mgmt_1, 0)
 
 check = False
 chk_memory = sysv_ipc.SharedMemory(1219, flags=01000, size=4, mode=0600)
@@ -34,38 +30,9 @@ class ListQueue:
     def qsize(self):
         return len(self.my_list)
 
-def read_byte(adr):
-    return bus.read_byte_data(address, adr)
-
-def read_word(adr):
-    high = bus.read_byte_data(address, adr)
-    low = bus.read_byte_data(address, adr + 1)
-    val = (high << 8) + low
-    return val
-
-def read_word_2c(adr):
-    val = read_word(adr)
-    if (val >= 0x8000):
-        return -((65535 - val) + 1)
-    else:
-        return val
-
 def create_time():
     now = datetime.datetime.today().strftime("%y%m%d_%H%M%S")
     return now
-
-def detect_impact():
-    gyro_xout = read_word_2c(0x43)
-    gyro_yout = read_word_2c(0x45)
-    gyro_zout = read_word_2c(0x47)
-
-    x_scale = gyro_xout / 131.0
-    y_scale = gyro_yout / 131.0
-    z_scale = gyro_zout / 131.0
-        
-    #print(y_scale)
-    if (2.5 < y_scale):
-        return True
 
 def impact():
     t = create_time()
@@ -95,7 +62,6 @@ def impact():
        #     print("memory: %s" % fin)  
         #    video_mixing(t)
          #   break
-
 
 def video_mixing(t):
     print("Start")
@@ -245,7 +211,9 @@ if __name__=="__main__":
 
 	while True:
 		check = detect_impact()
-		if check == True:
+		print(check)
+		if check == 'Impt':
+			print(check)
 			impact()
 		else:
 			continue
