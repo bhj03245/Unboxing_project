b053719e5e17425252e048d776c06c5c3a963664
dkrmrm878 <dkrmrm878@gmail.com>
2020-06-21 03:13:04 +0900

    Update 'receive mode from app'

--- Web/apkCtrl/reportImpt_apk.php
+++ Web/apkCtrl/reportImpt_apk.php
@@ -1,10 +1,20 @@
 <?php
-	$trig = $_POST['request_impt'];
+	require_once("../db.php");
+	$conn = db_connect();
+	mysqli_query("set names utf8");	
+	
+	$mode = $_POST['request_impt'];
+
 	$fp = fopen("impt_data.txt", "r");
 	$fr = fread($fp, 10);
 	
-	if($trig != null){
-		echo $fr;	
+	if($mode == True){
+		echo $fr;
+		$result = $conn->query("UPDATE users SET users_mode = 'NORM' WHERE users_num = 1");
 	}
+	else if($mode == null){
+		$result = $conn->query("UPDATE users SET users_mode = 'PARK' WHERE users_num = 1");
+	}
+
 	fclose($fp);
 ?>
