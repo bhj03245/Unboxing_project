df3c6470911a778fbec54d3905643d40cf9802ca
wk1219 <sjms1215@naver.com>
2020-06-27 18:19:54 +0900

    Update RPI_UI.py

--- UI/RPI_UI.py
+++ UI/RPI_UI.py
@@ -1,18 +1,42 @@
 # -*-coding:utf8;-*-
 
 import os
+from kivy.uix.image import Image
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.switch import Switch
 from kivy.core.text import LabelBase
+from kivy.uix.switch import Switch
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.video import Video
+from kivy.uix.camera import Camera
+import cv2
 
-LabelBase.register(name='malgun',
-                   fn_regular='malgun.ttf')
+
+LabelBase.register(name="malgun",
+                   fn_regular="malgun.ttf")
+
+class KivyCamera(Screen):
+    def __init__(self, capture, fps, **kwargs):
+        super(KivyCamera, self).__init__(**kwargs)
+        self.capture = capture
+        Clock.schedule_interval(self.update, 1.0 / fps)
+
+    def update(self, dt):
+        ret, frame = self.capture.read()
+        if ret:
+            buf1 = cv2.flip(frame, 0)
+            buf = buf1.tostring()
+            image_texture = Texture.create(
+                size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            # display image from the texture
+            self.texture = image_texture
 
 class Main(Screen):
     pass
-
 
 class Menu(Screen):
     pass
@@ -32,17 +56,32 @@ class Setting(Screen):
         else:
             print('Switch is OFF')
 
-class Video_list(Screen):
+class VideoWidget(Screen):
     pass
 
+
+class Video_list(Screen):
+    def video_list(selfself, obj, value):
+        if value == 'Norm':
+            VideoWidget()
+        elif value == 'Impt':
+            VideoWidget()
+        elif value == 'Park':
+            VideoWidget()
+        elif value == 'Manl':
+            VideoWidget()
 
 class WindowManager(ScreenManager):
     pass
 
-
 class MyApp(App):
     def build(self):
+        self.capture = cv2.VideoCapture(0)
+        self.my_camera = KivyCamera(capture=self.capture, fps=30)
         return ui
+
+    def on_stop(self):
+        self.capture.release()
 
 
 ui = Builder.load_file("UI.kv")
