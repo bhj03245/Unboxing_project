3e4f2e761ca48f3df0e8a1c5ca41d2a8f9fec549
dkrmrm878 <dkrmrm878@gmail.com>
2020-06-29 17:54:33 +0900

    Update user_auth

--- Web/apkCtrl/user_auth_apk.php
+++ Web/apkCtrl/user_auth_apk.php
@@ -6,16 +6,25 @@
    
     $u_id=$_POST['id'];
     $u_pw=$_POST['pw'];
-    
+	
+    $sql = "SELECT IF(strcmp(users_pwd, '$u_pw'),0,1) pw_chk FROM users WHERE users_id='$u_id'";
 
-    $result=$conn->query("SELECT * FROM users WHERE users_id='$u_id' and users_pwd='$u_pw'");
+    $result=mysqli_query($conn, $sql);
     
-   
     if($result){
-        echo "1";
-    }else{
-        echo "0";
-	    echo mysql_errno($conn);
-    }
-
+  		$row = mysqli_fetch_array($result);
+		if(is_null($row['pw_chk'])){
+			echo 'ID를 다시 입력해 주세요.';
+		}
+		else if($row['pw_chk'] == 0){
+			echo $row['pw_chk'];
+		}
+		else if($row['pw_chk'] == 1){
+			echo $row['pw_chk'];
+			$_SESSION['valid_user'] = $id;
+		}
+	}else{
+		echo mysql_errno($conn);
+	}
+	mysqli_close($conn);
 ?>
