a91def9ca46f779a09520a64f9b9e40dfbb5e1b7
dkrmrm878 <dkrmrm878@gmail.com>
2020-08-22 13:52:52 +0900

    Fix type

--- Web/apkCtrl/reportSet_apk.php
+++ Web/apkCtrl/reportSet_apk.php
@@ -10,24 +10,22 @@
     $u_content=$_POST['content'];
 
     $result = $conn->query("SELECT * FROM msg");
-    //echo "gg";
     
     if(mysqli_num_rows($result) > 0){
         $update = $conn->query("UPDATE msg SET msg_phone='$u_phone', msg_content='$u_content' WHERE msg_num=1)");
-	if($update==TRUE){
-	    echo "Update";
-	}else{
-	    echo mysql_errno($conn);
-        }
-    }else{
-	$add = $conn -> query("INSERT INTO msg VALUES(1, '$u_phone', '$u_content', 'trigger')");
-	if($add==TRUE){
-	    echo "Insert";
-	}else{
+        if($update==TRUE){
+            echo "Update";
+        }else{
             echo mysql_errno($conn);
-	}
+            }
+        }else{
+        $add = $conn -> query("INSERT INTO msg VALUES(1, '$u_phone', '$u_content', 'trigger')");
+        if($add==TRUE){
+            echo "Insert";
+        }else{
+                echo mysql_errno($conn);
+        }
     }
-
 
     mysqli_close($conn);
 
